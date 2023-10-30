e47e9f8b02421cb6f2c3d397321213399f59e906
ReeceRenninger <renningerreece@gmail.com>
2023-10-30 13:41:04 -0700

    got emojis running in text block assigned to main grid

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,30 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>() 
+            {
+            "🐵", "🐵",
+            "🐳","🐳",
+            "🐙","🐙",
+            "🐸","🐸",
+            "🐶","🐶",
+            "🦞","🦞",
+            "🦇","🦇",
+            "🦉","🦉",
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
