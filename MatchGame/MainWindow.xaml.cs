e50eb0d2591b41b59abb64251e20223330b7e51a
ReeceRenninger <renningerreece@gmail.com>
2023-10-31 18:57:02 -0700

    matching game working with timer method

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,15 +15,34 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -42,11 +61,18 @@ namespace MatchGame
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                if (textBlock.Name != "timeTextBlock") // adding the time text block broke our page so we need to add a condition so it doesn't target the time text block
+                {
+                textBlock.Visibility = Visibility.Visible;
                 int index = random.Next(animalEmoji.Count);
                 string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+                }
             }
+            timer.Start(); //invoke the timer once the first text block is clicked?
+            tenthsOfSecondsElapsed = 0; // setting the timer text to 0
+            matchesFound = 0; // resetting matchesFound to 0
         }
 
         // global variables for the TextBLock_MouseDown
@@ -62,10 +88,11 @@ namespace MatchGame
                 lastTextBlockClicked = textBlock;
                 findingMatch = true;
             }
-        else if ( textBlock.Text == lastTextBlockClicked.Text ) // if the textBlock text that was clicked MATCHES the last textBlock clicked text
+        else if ( textBlock.Text == lastTextBlockClicked.Text)
             {
-                textBlock.Visibility = Visibility.Hidden;
-                findingMatch = false;
+                matchesFound++; // increase matchesFound if text block texts matched
+                textBlock.Visibility = Visibility.Hidden; // we hide the blocks as they are matched
+                findingMatch = false; // ** Not sure what this is doing???
             }
         else
             {
@@ -74,6 +101,14 @@ namespace MatchGame
             }
         }
 
- 
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s"); // setting the text of the time text block
+            if (matchesFound == 8) // if all matches are found, we restart the game
+            {
+                SetUpGame();
+            }
+        }
     }
 }
